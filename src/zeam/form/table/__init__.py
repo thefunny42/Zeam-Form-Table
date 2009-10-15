253516be7cfda9e030e6554b4273c81e14000188
Sylvain Viollon <thefunny@gmail.com>
2009-10-15 18:28:43 +0200

    Define an IZeamFormTableAPI, update interfaces.

--- src/zeam/form/table/__init__.py
+++ src/zeam/form/table/__init__.py
@@ -3,3 +3,6 @@ from zeam.form.base import *
 
 from zeam.form.table.actions import TableActions
 from zeam.form.table.form import TableForm
+
+from zeam.form.table.interfaces import IZeamFormTableAPI
+__all__ = list(IZeamFormTableAPI)
--- src/zeam/form/table/interfaces.py
+++ src/zeam/form/table/interfaces.py
@@ -1,13 +1,33 @@
 
 from zope import interface
 
-from zeam.form.base.interfaces import IFormCanvas
+from zeam.form.base.interfaces import IFormCanvas, IForm
+from zeam.form.base.interfaces import IZeamFormBaseAPI
 
 
 class ITableFormCanvas(IFormCanvas):
+    """Base form behavior for forms working on more than one content
+    at a time.
+    """
 
     lines = interface.Attribute(u"Widgets lines")
 
     def getItems():
         """Return the list of contents.
         """
+
+
+class ITableForm(IForm):
+    """A form which is able to work on more than one item at a time.
+    """
+
+
+
+class IZeamFormTableAPI(IZeamFormBaseAPI):
+    """Public API of zeam.form.table
+    """
+
+    TableForm = interface.Attribute(
+        u"A form able to work on more on more than one item at a time")
+    TableActions = interface.Attribute(
+        u"Action being executed on more than one content at a time")
